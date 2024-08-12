faf86e1a4dc190a5208497df4afa3598eea6a168
zkyim <113043246+zkyim@users.noreply.github.com>
2024-08-12 22:09:51 +0300

    Add files via upload

--- includes/templets/header.php
+++ includes/templets/header.php
@@ -1,174 +1,45 @@
-<?php 
-    $HTTP = explode('/', $_SERVER['PHP_SELF']);
-    $HTTP = end($HTTP);
-
-    $css = 'layout/css/';
-    function setActive ($name='home') {
-      global $pageName;
-      if (isset($pageName) && $pageName == $name) {echo 'active';}
-    }
+<?php
+$css = 'layout/css/';
 ?>
 
 <!DOCTYPE html>
-    <?php 
-    if (isset($_COOKIE['style']['maincolor']) && isset($_COOKIE['style']['maincoloralt'])) {
-        echo '<html lang="en" style="--main-color:'.$_COOKIE['style']['maincolor'].';--main-color-alt:'.$_COOKIE['style']['maincoloralt'].';">';
-    }else {
-        echo '<html lang="en">';
-    }
-    ?>
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<html lang="en" dir="rtl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title> <?php echo GetTitle(); ?> </title>
+        <link rel="icon" href="">
 
-    <link rel="stylesheet" href="layout/css/all.min.css" />
-    <link rel="stylesheet" href="layout/css/framework.css" />
-    <link rel="stylesheet" href="layout/css/master.css" />
-    <link rel="stylesheet" href="<?php echo $css; echo GetLinkCss();?>">
+        <link rel="stylesheet" href="layout/css/all.min.css">
+        <link rel="stylesheet" href="layout/css/normalize.css">
+        <link rel="stylesheet" href="layout/css/master.css">
+        <link rel="stylesheet" href="<?php echo $css; echo GetLinkCss();?>">
 
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div class="page d-flex">
-      <div class="sidebar bg-white p-20 p-relative <?php if (isset($_COOKIE['style']['designlinks'])) {if($_COOKIE['style']['designlinks']=='hidelinks'){echo'hide';}elseif($_COOKIE['style']['designlinks']!='sidelinks'){echo 'close';}} ?>">
-            <div class="open-icon">
-              <i class="fa-solid fa-bars fa-fw"></i>
-            </div>
-        <h3 class="p-relative txt-c mt-0">مستقبلك</h3>
-        <ul>
-          <a href="dashboard.php">
-            <li class="<?php setActive ('dashboard') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-              <i class="fa-regular fa-chart-bar fa-fw"></i>
-              <span>لوحة التحكم</span>
-            </li>
-          </a>
-          <a href="myaccount.php">
-            <li  class="<?php setActive ('myaccount') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-              <i class="fa-regular fa-user fa-fw"></i>
-              <span>حسابي</span>
-            </li>
-          </a>
-          <a href="mytests.php">
-            <li  class="<?php setActive ('mytests') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-              <i class="fa-regular fa-rectangle-list fa-fw"></i>
-              <span>إختباراتي</span>
-            </li>
-          </a>
-          <a href="mynotes.php">
-            <li  class="<?php setActive ('mynotes') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-            <i class="fa-regular fa-credit-card fa-fw"></i>
-              <span>ملاحظاتي</span>
-            </li>
-          </a>
-          <a href="challenge.php" >
-            <li class="<?php setActive ('challenge') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-              <i class="fa-solid fa-diagram-project fa-fw"></i>
-              <span>التحديات</span>
-            </li>
-          </a>
-          <a href="courses.php" >
-            <li class="<?php setActive ('courses') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-              <i class="fa-solid fa-graduation-cap fa-fw"></i>
-              <span>الدورات</span>
-            </li>
-          </a>
-          <a href="settings.php">
-            <li class="<?php setActive ('settings') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-              <i class="fa-solid fa-gear fa-fw"></i>
-              <span>الإعدادات</span>
-            </li>
-          </a>
-          <a href="aboutus.php">
-            <li class="<?php setActive () ?> d-flex align-center fs-14 c-black rad-6 p-10">
-              <i class="fa-regular fa-address-card fa-fw"></i>
-              <span>لمحة عنا</span>
-            </li>
-          </a>
-
-        </ul>
-      </div>
-      <div class="content w-full">
-        <!-- Start Head -->
-        <div class="head bg-white p-15 between-flex">
-          <!-- if top-head colse add close class -->
-          <div class="top-head <?php if (isset($_COOKIE['style']['designlinks'])){if($_COOKIE['style']['designlinks']!='toplinks'){echo'close';}}else{echo'close';} ?>">
-            <h3 class="p-relative txt-c mt-0">مستقبلك</h3>
-            <ul>
-              <a href="dashboard.php">
-                <li class="<?php setActive ('dashboard') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                  <i class="fa-regular fa-chart-bar fa-fw"></i>
-                  <span>لوحة التحكم</span>
-                </li>
-              </a>
-              <a href="myaccount.php">
-                <li  class="<?php setActive ('myaccount') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                  <i class="fa-regular fa-user fa-fw"></i>
-                  <span>حسابي</span>
-                </li>
-              </a>
-              <a href="mytests.php">
-                <li  class="<?php setActive ('mytests') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                  <i class="fa-regular fa-rectangle-list fa-fw"></i>
-                  <span>إختباراتي</span>
-                </li>
-              </a>
-              <a href="mynotes.php">
-                <li  class="<?php setActive ('mynotes') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                <i class="fa-regular fa-credit-card fa-fw"></i>
-                  <span>ملاحظاتي</span>
-                </li>
-              </a>
-              <a href="challenge.php" >
-                <li class="<?php setActive ('challenge') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                  <i class="fa-solid fa-diagram-project fa-fw"></i>
-                  <span>التحديات</span>
-                </li>
-              </a>
-              <a href="courses.php" >
-                <li class="<?php setActive ('courses') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                  <i class="fa-solid fa-graduation-cap fa-fw"></i>
-                  <span>الدورات</span>
-                </li>
-              </a>
-              <a href="settings.php">
-                <li class="<?php setActive ('settings') ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                  <i class="fa-solid fa-gear fa-fw"></i>
-                  <span>الإعدادات</span>
-                </li>
-              </a>
-              <a href="aboutus.php">
-                <li class="<?php setActive () ?> d-flex align-center fs-14 c-black rad-6 p-10">
-                  <i class="fa-regular fa-address-card fa-fw"></i>
-                  <span>لمحة عنا</span>
-                </li>
-              </a>
-
-            </ul>
-          </div>
-          <div class="icons d-flex align-center">
-            <?php 
-            if ($_SESSION['Avatar'] == '') {
-                echo '<img src="'.$upload.'imags/Avatar/defaultImg.png" alt="">';
-            }else {
-                echo '<img src="'.$upload.'imags/Avatar/'.$_SESSION['Avatar'].'" alt="">';
-            }
-            ?>
-          </div>
-
-        </div>
-        <!-- Start CheckOnline -->
-        <div class="check-online"></div>
-        <!-- End CheckOnline -->
-        <!-- Start Notifications -->
-        <ul class="notifications"></ul>
-        <!-- End Notifications -->
         
-        <!-- End Head -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet">
+        
+        <title> <?php GetTitle() ?> </title>
+    </head>
+    <body>
 
-        <div class="container-content">
+        <header>
+            <div class="container">
+                <div class="logo">مشروع</div>
+                <div class="dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="links">
+                    <ul>
+                        <li><a href="<?php Getsrc1 () ?>"> <?php Getnamesrc1 () ?> </a></li>
+                        <li><a href="<?php Getsrc2 () ?>"> <?php Getnamesrc2 () ?> </a></li>
+                        <li><a href="">لمحة عنا</a></li>
+                        <li><a href="logout.php">خروج</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
--- layout/js/master.js
+++ layout/js/master.js
@@ -1,99 +1,29 @@
-// let upload = '../student/upload/';
-
-// ====================================== Start Hide Sidebar =========================================
-let sidebar = document.querySelector('.sidebar');
-let openIconSidebar = document.querySelector('.sidebar .open-icon');
-sidebar.addEventListener('click', e => {
-    e.stopPropagation();
-});
-openIconSidebar.addEventListener('click', e => {
-    sidebar.classList.toggle('open');
-});
-document.addEventListener('click', e => {
-    if (e.target !== sidebar || e.target !== openIconSidebar) {sidebar.classList.remove('open');}
-})
-// ====================================== End Hide Sidebar ===========================================
-
-// ====================================== Start Toast =========================================
-// paramiter1 of type 1-success or 2-error or 3-warning or 4-info
-// paramiter2 of text
-let notifications = document.querySelector(".notifications");
-function removeToast (toast) {
-    toast.classList.add('hide');
-    setTimeout(() => {toast.remove()}, 500);
+let up = document.querySelector(".up")
+window.onscroll = () => {
+    this.scrollY >= 500 ? up.classList.add("show") : up.classList.remove("show")
 }
-function createToast (type, text) {
-    let toastDetails = {
-        timer : 4000,
-        success: {
-            icon : "fa-circle-check"
-        },
-        error: {
-            icon : "fa-circle-xmark"
-        },
-        warning: {
-            icon : "fa-triangle-exclamation"
-        },
-        info: {
-            icon : "fa-circle-info"
-        }
-    };
-    let {icon} = toastDetails[type];
-    let toast = document.createElement('li');
-    toast.className = `toast ${type}`;
-    toast.innerHTML = 
-    `
-    <div class="column">
-        <i class="fa-solid ${icon}"></i>
-        <span>${text}</span>
-    </div>
-    <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>
-    `;
-    notifications.appendChild(toast);
-    setTimeout(() => removeToast(toast), toastDetails.timer)
-}
-// ====================================== End Toast =========================================
-// ====================================== Start Check Online ================================
-let checkOnlineCont = document.querySelector('.check-online');
-function removeCheckOnline () {
-    checkOnlineCont.classList.add('hide');
-    setTimeout(() => {checkOnlineCont.innerHTML = ''}, 500);
-}
-function checkOnline (work = 'nowork') {
-    let text;
-    let mode;
-    if (window.navigator.onLine) {
-        checkOnlineCont.classList.remove('offline');
-        text = 'أنت متصل بالإنترنت';
-        mode = 'online';
-    }else {
-        checkOnlineCont.classList.add('offline');
-        text = 'أنت غير متصل بالإنترنت';
-        mode = 'offline';
-    }
-    if (work != 'nowork') {
-        checkOnlineCont.innerHTML =
-        `
-        <div class="content between-flex p-relative">
-            <div class="column">
-                <i class="fa-solid fa-wifi p-relative"></i>
-                <span class="mr-10"> ${text} </span>
-            </div>
-            <i class="fa-solid fa-xmark" onclick="removeCheckOnline ()"></i>
-        </div>
-        `;
-    }
-    setTimeout(() => removeCheckOnline(), 5000);
-    return mode;
-}
-// ====================================== End Check Online =========================================
-let textareas = document.querySelectorAll("textarea");
-resizeTextarea(textareas);
-function resizeTextarea (textareas) {
-    textareas.forEach( element => {
-        element.addEventListener('keyup', (e) => {
-            // element.style.height = '42px';
-            element.style.height = (e.target.scrollHeight) + 'px';
-        });
+up.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
 }
+
+let butNav = document.querySelector("header .dots");
+let links = document.querySelector("header .links");
+butNav.addEventListener("click", e => {
+    e.stopPropagation();
+});
+links.addEventListener("click", e => {
+    e.stopPropagation();
+});
+butNav.addEventListener('click', e => {
+    butNav.classList.toggle('active');
+    links.classList.toggle('active');
+});
+document.addEventListener('click', e => {
+    if (e.target != butNav || e.target!= links) {
+        butNav.classList.remove('active');
+        links.classList.remove('active');
+    }
+});
